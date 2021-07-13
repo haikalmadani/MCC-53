53db3498e88064a3333b23171209a0497b534cdd
haikalmadani <muhammadhaikalmadani@gmail.com>
2021-07-13 11:26:48 +0700

    Update genap.cs

--- genap.cs
+++ genap.cs
@@ -4,21 +4,9 @@ namespace Genap {
 					
 	public class Program
 	{
-		public void Main()
-		{
-			int[] array = new int [10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-			int i = 1;
-			Program program = new Program();
-			for (i = 1; i <= array.Length; i++){
-				if(i % 2 == 0){
-					Console.WriteLine(i);
-					
-				}
-				else
-				{
-					Console.WriteLine("Ganjil");
-				}
-			}
+        public static void Main() {
+			Console.WriteLine("NGETEST COBA BISA GA SI SEBENERNYA");
 		}
-	}
+    }
 }
+
